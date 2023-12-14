a8ff6fca2cf16df95eefad28827440b573b362de
mariajacynthia <143603331+mariajacynthia@users.noreply.github.com>
2023-12-14 13:14:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('red').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('green').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+});
+
+document.getElementById('blue').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('white').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'white';
+});
